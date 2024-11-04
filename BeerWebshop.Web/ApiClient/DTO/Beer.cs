94c397a3a8bd3ec90dd809ff5d6bf2021a092309
MandeMads <10201992@ucn.dk>
2024-11-04 12:02:31 +0100

    fisx

--- BeerWebshop.Web/ApiClient/DTO/Beer.cs
+++ BeerWebshop.Web/ApiClient/DTO/Beer.cs
@@ -2,7 +2,7 @@
 {
     public class Beer
     {
-        public required int Id { get; set; }
+        public int Id { get; set; }
         public required string Name { get; set; }
         public required string Brewery { get; set; }
         public required float Price { get; set; }
