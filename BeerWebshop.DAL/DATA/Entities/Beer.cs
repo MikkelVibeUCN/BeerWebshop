befcc49cf8e3dd02486456b07c11bff38091292e
Mikkel <10503091@ucn.dk>
2024-11-04 11:00:32 +0100

    Created entities in DAL layer

---
+++ BeerWebshop.DAL/DATA/Entities/Beer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerWebshop.DAL.DATA.Entities
+{
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string Brewery { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public int stock { get; set; }
+        public float ABV { get; set; }
+        public string Category { get; set; }
+        public int? Id { get; set; }
+        
+        public Beer(string name, string brewery, float price, string description, int stock, float abv, string category, int? id = null)
+        {
+            Name = name;
+            Brewery = brewery;
+            Price = price;
+            Description = description;
+            this.stock = stock;
+            ABV = abv;
+            Category = category;
+            Id = id;
+        }
+
+    }
+}
