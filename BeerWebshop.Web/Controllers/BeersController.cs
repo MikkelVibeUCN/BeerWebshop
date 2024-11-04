51de0ff6e83acac2446bfeaee4689ca6d51a9523
MandeMads <10201992@ucn.dk>
2024-11-04 11:58:10 +0100

    Beer view

---
+++ BeerWebshop.Web/Controllers/BeersController.cs
@@ -0,0 +1,25 @@
+﻿using BeerWebshop.Web.ApiClient;
+using BeerWebshop.Web.ApiClient.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerWebshop.Web.Controllers
+{
+    public class BeersController : Controller
+    {
+        private readonly IRestClient _restClient;
+
+        public BeersController(IRestClient restClient)
+        {
+            _restClient = restClient;
+        }
+
+        // GET: BeerController
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Beer> beers = _restClient.GetTenLatestBeers();
+            return View(beers);
+        }
+
+    }
+}
