0e29d28b443c246440148ca54e24500963d2a48b
MandeMads <10201992@ucn.dk>
2024-11-04 11:08:18 +0100

    Setup for beer page

---
+++ BeerWebshop.Web/ApiClient/IRestClient.cs
@@ -0,0 +1,12 @@
+﻿using BeerWebshop.Web.ApiClient.DTO;
+
+namespace BeerWebshop.Web.ApiClient
+{
+    public interface IRestClient
+    {
+        IEnumerable<Beer> GetTenLatestBeers();
+        Beer GetBeerFromId(int id);
+        int AddNewBeer(Beer beer);
+
+    }
+}
---
+++ BeerWebshop.Web/Controllers/BeerController.cs
@@ -0,0 +1,92 @@
+﻿using BeerWebshop.Web.ApiClient;
+using BeerWebshop.Web.ApiClient.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerWebshop.Web.Controllers
+{
+    public class BeerController : Controller
+    {
+        private readonly IRestClient _restClient;
+
+        public BeerController(IRestClient restClient)
+        {
+            _restClient = restClient;
+        }
+
+        // GET: BeerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BeerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(_restClient.GetBeerFromId(id));
+        }
+
+        // GET: BeerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BeerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Beer beer)
+        {
+            try
+            {
+                _restClient.AddNewBeer(beer);           return Redirect("/home/index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BeerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BeerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BeerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BeerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
