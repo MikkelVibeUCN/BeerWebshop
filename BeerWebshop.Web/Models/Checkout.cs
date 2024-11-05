ab01474af3636942cc2a901a6f5a0349f014d5c4
MandeMads <10201992@ucn.dk>
2024-11-05 13:26:53 +0100

    qol

--- BeerWebshop.Web/Models/Checkout.cs
+++ BeerWebshop.Web/Models/Checkout.cs
@@ -2,8 +2,8 @@
 {
     public class Checkout
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public string PostalCode { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
