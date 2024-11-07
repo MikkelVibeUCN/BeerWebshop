dc9b99acdbfabf996534d17c39e478319feae0f9
Andershsm <10503096@ucn.dk>
2024-11-07 10:26:05 +0100

    ProductDTO from DAL to API.DTO

---
+++ BeerWebshop.RESTAPI/DTO/ProductDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace BeerWebshop.RESTAPI.DTO;
+
+public class ProductDTO
+{
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Brewery { get; set; }
+        public required float Price { get; set; }
+        public required string Description { get; set; }
+        public required int Stock { get; set; }
+        public float ABV { get; set; }
+        public required string Type { get; set; }
+        public string ImageUrl { get; set; }
+
+}
