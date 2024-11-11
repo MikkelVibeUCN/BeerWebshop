d30dfc1adf56f206be9163f630add67fdc26b725
ThomasUCN <10503119@ucn.dk>
2024-11-11 13:29:46 +0100

    MVC for user story 7

---
+++ BeerWebshop.Web/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeerWebshop.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateAccount() => View();
+    }
+}
---
+++ BeerWebshop.Web/Services/AccountService.cs
@@ -0,0 +1,19 @@
+﻿using BeerWebshop.APIClientLibrary.ApiClient.Client;
+using BeerWebshop.APIClientLibrary.ApiClient.DTO;
+
+namespace BeerWebshop.Web.Services
+{
+    public class AccountService
+    {
+        private ICustomerpiClient _accountApiClient;
+
+        public AccountService(IAccountAPIClient accountApiClient)
+        {
+            _accountApiClient = accountApiClient;
+        }
+        public async Task<CustomerDTO> CreateCustomer(CustomerDTO customer)
+        {
+
+        }
+    }
+}
