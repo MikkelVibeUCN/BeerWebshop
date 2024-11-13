c75dade0a97b08ea9b6f0030f9fe69b5463ffe97
Mikkel <10503091@ucn.dk>
2024-11-13 12:00:19 +0100

    Fixed dao

--- BeerWebshop.DAL/DATA/Entities/Customer.cs
+++ BeerWebshop.DAL/DATA/Entities/Customer.cs
@@ -5,8 +5,6 @@ public class Customer
 	public int Id { get; set; }
 	public string? Name { get; set; }
 	public string? Address { get; set; }
-	public string? ZipCode { get; set; }
-	public string? City { get; set; }
 	public string? Email { get; set; }
 	public string? Password { get; set; }
 	public string? Phone { get; set; }
