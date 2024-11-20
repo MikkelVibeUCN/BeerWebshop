49c2f4fa839f476633fd0cd87baa733291d1f91b
MandeMads <10201992@ucn.dk>
2024-11-20 08:51:22 +0100

    ændringer i client

--- BeerWebshop.DesktopClient/MainForm.Designer.cs
+++ BeerWebshop.DesktopClient/MainForm.Designer.cs
@@ -28,34 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            grpFrontpage = new GroupBox();
             splitContainer1 = new SplitContainer();
             grpOrders = new GroupBox();
-            btnPreviousOrders = new Button();
             btnOpenOrders = new Button();
             grpCrudProducts = new GroupBox();
             btnAddProducts = new Button();
             btnViewProducts = new Button();
             button1 = new Button();
-            grpFrontpage.SuspendLayout();
+            grpFrontpage = new GroupBox();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel1.SuspendLayout();
             splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
             grpOrders.SuspendLayout();
             grpCrudProducts.SuspendLayout();
+            grpFrontpage.SuspendLayout();
             SuspendLayout();
-            // 
-            // grpFrontpage
-            // 
-            grpFrontpage.Controls.Add(splitContainer1);
-            grpFrontpage.Dock = DockStyle.Top;
-            grpFrontpage.Location = new Point(0, 0);
-            grpFrontpage.Name = "grpFrontpage";
-            grpFrontpage.Size = new Size(782, 754);
-            grpFrontpage.TabIndex = 0;
-            grpFrontpage.TabStop = false;
-            grpFrontpage.Text = "BeerWebshop Admin Side";
             // 
             // splitContainer1
             // 
@@ -76,29 +64,18 @@
             // 
             // grpOrders
             // 
-            grpOrders.Controls.Add(btnPreviousOrders);
             grpOrders.Controls.Add(btnOpenOrders);
             grpOrders.Dock = DockStyle.Top;
             grpOrders.Location = new Point(0, 0);
             grpOrders.Name = "grpOrders";
-            grpOrders.Size = new Size(258, 712);
+            grpOrders.Size = new Size(258, 500);
             grpOrders.TabIndex = 0;
             grpOrders.TabStop = false;
             grpOrders.Text = "Ordrer";
             // 
-            // btnPreviousOrders
-            // 
-            btnPreviousOrders.BackColor = Color.Gray;
-            btnPreviousOrders.Location = new Point(43, 461);
-            btnPreviousOrders.Name = "btnPreviousOrders";
-            btnPreviousOrders.Size = new Size(160, 148);
-            btnPreviousOrders.TabIndex = 0;
-            btnPreviousOrders.Text = "&Tidligere ordrer";
-            btnPreviousOrders.UseVisualStyleBackColor = false;
-            // 
             // btnOpenOrders
             // 
-            btnOpenOrders.BackColor = Color.LightPink;
+            btnOpenOrders.BackColor = Color.Beige;
             btnOpenOrders.Location = new Point(43, 119);
             btnOpenOrders.Name = "btnOpenOrders";
             btnOpenOrders.Size = new Size(160, 148);
@@ -115,14 +92,14 @@
             grpCrudProducts.Dock = DockStyle.Top;
             grpCrudProducts.Location = new Point(0, 0);
             grpCrudProducts.Name = "grpCrudProducts";
-            grpCrudProducts.Size = new Size(514, 712);
+            grpCrudProducts.Size = new Size(514, 715);
             grpCrudProducts.TabIndex = 0;
             grpCrudProducts.TabStop = false;
             grpCrudProducts.Text = "Produkter";
             // 
             // btnAddProducts
             // 
-            btnAddProducts.BackColor = Color.LightGreen;
+            btnAddProducts.BackColor = Color.Beige;
             btnAddProducts.Location = new Point(31, 119);
             btnAddProducts.Name = "btnAddProducts";
             btnAddProducts.Size = new Size(171, 148);
@@ -133,7 +110,7 @@
             // 
             // btnViewProducts
             // 
-            btnViewProducts.BackColor = SystemColors.Highlight;
+            btnViewProducts.BackColor = Color.Beige;
             btnViewProducts.Location = new Point(315, 119);
             btnViewProducts.Name = "btnViewProducts";
             btnViewProducts.Size = new Size(166, 148);
@@ -151,36 +128,46 @@
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // grpFrontpage
+            // 
+            grpFrontpage.Controls.Add(splitContainer1);
+            grpFrontpage.Dock = DockStyle.Top;
+            grpFrontpage.Location = new Point(0, 0);
+            grpFrontpage.Name = "grpFrontpage";
+            grpFrontpage.Size = new Size(782, 754);
+            grpFrontpage.TabIndex = 0;
+            grpFrontpage.TabStop = false;
+            grpFrontpage.Text = "BeerWebshop Admin Side";
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(15F, 37F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(782, 753);
+            ClientSize = new Size(782, 403);
             Controls.Add(grpFrontpage);
             Font = new Font("Segoe UI", 16F);
             Margin = new Padding(4);
             Name = "MainForm";
             Text = "Forside";
-            grpFrontpage.ResumeLayout(false);
             splitContainer1.Panel1.ResumeLayout(false);
             splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
             grpOrders.ResumeLayout(false);
             grpCrudProducts.ResumeLayout(false);
+            grpFrontpage.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private GroupBox grpFrontpage;
         private SplitContainer splitContainer1;
         private GroupBox grpCrudProducts;
-        private GroupBox grpOrders;
         private Button btnAddProducts;
         private Button btnViewProducts;
         private Button button1;
-        private Button btnPreviousOrders;
+        private GroupBox grpFrontpage;
+        private GroupBox grpOrders;
         private Button btnOpenOrders;
     }
 }
