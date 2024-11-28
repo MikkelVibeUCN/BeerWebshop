14864a59410b1a619bad34d5b3e2e9c4cfe6e5cc
Andershsm <10503096@ucn.dk>
2024-11-28 13:55:41 +0100

    Password i DesktopClient maskeret.

--- BeerWebshop.DesktopClient/LoginForm.cs
+++ BeerWebshop.DesktopClient/LoginForm.cs
@@ -11,7 +11,35 @@ namespace BeerWebshop.DesktopClient
         public LoginForm()
         {
             _accountAPIClient = new AccountAPIClient("https://localhost:7244/api/v1/");
+            
             InitializeComponent();
+
+            LoadPictures();
+        }
+
+        private void LoadPictures()
+        {
+            pictureBox.Image = null;
+            var imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Anders_Hejlsberg.jpg/330px-Anders_Hejlsberg.jpg";
+            pictureBox.Load(imageUrl);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+
+            pictureBox1.Image = null;
+            var imageUrl1 = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Anders_Hejlsberg_at_PDC2008.jpg/300px-Anders_Hejlsberg_at_PDC2008.jpg";
+            pictureBox1.Load(imageUrl1);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            pictureBox2.Image = null;
+            var imageUrl2 = "https://media.licdn.com/dms/image/C4E03AQGRfBRQcoI7Kw/profile-displayphoto-shrink_800_800/0/1624267235021?e=2147483647&v=beta&t=qY7bfscxxPll-_cXR4WMI5M4BtKB79kvNaV_C65yXY4";
+            pictureBox2.Load(imageUrl2);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            pictureBox3.Image = null;
+            var imageUrl3 = "https://th.bing.com/th/id/OIP.Sh5-0947wcN175xUfsPzDwAAAA?rs=1&pid=ImgDetMain";
+            pictureBox3.Load(imageUrl3);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+
+
         }
 
         private async void btnLogin_Click(object sender, EventArgs e)
