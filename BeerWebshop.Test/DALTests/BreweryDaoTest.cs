1bd50618d9e61f0e2cfee85ce78766bc4838fb68
Ahsm123 <10503096@ucn.dk>
2024-12-16 10:42:07 +0100

    AccountDao. SQL queries syntax

--- BeerWebshop.Test/DALTests/BreweryDaoTest.cs
+++ BeerWebshop.Test/DALTests/BreweryDaoTest.cs
@@ -6,93 +6,93 @@ namespace BeerWebshop.Test.DALTests;
 [TestFixture]
 public class BreweryDaoTest
 {
-	private BreweryDAO _breweryDAO;
-	private readonly string _testSuffix = "_Test";
-	private readonly List<int> _breweryIdsCreated = new();
+    private BreweryDAO _breweryDAO;
+    private readonly string _testSuffix = "_Test";
+    private readonly List<int> _breweryIdsCreated = new();
 
-	[SetUp]
-	public void SetUp()
-	{
-		_breweryDAO = new BreweryDAO(DBConnection.ConnectionString());
-	}
+    [SetUp]
+    public void SetUp()
+    {
+        _breweryDAO = new BreweryDAO(DBConnection.ConnectionString());
+    }
 
-	[TearDown]
-	public async Task TearDownAsync()
-	{
-		foreach (var breweryId in _breweryIdsCreated)
-		{
-			await _breweryDAO.DeleteAsync(breweryId);
-		}
-	}
+    [TearDown]
+    public async Task TearDownAsync()
+    {
+        foreach (var breweryId in _breweryIdsCreated)
+        {
+            await _breweryDAO.DeleteAsync(breweryId);
+        }
+    }
 
-	[Test]
-	public async Task CreateAsync_WhenBreweryDoesNotExist_ShouldCreateBrewery()
-	{
-		var breweryName = $"Brewery{_testSuffix}";
-		var brewery = new Brewery { Name = breweryName, IsDeleted = false };
-
-		var breweryId = await _breweryDAO.CreateAsync(brewery);
-		_breweryIdsCreated.Add(breweryId);
-
-		Assert.That(breweryId, Is.GreaterThan(0));
-
-		var exception = Assert.ThrowsAsync<InvalidOperationException>(async () => await _breweryDAO.CreateAsync(brewery));
-		Assert.That(exception.Message, Is.EqualTo($"Brewery with name '{breweryName}' already exists."));
-	}
-
-	[Test]
-	public async Task CreateAsync_WhenBreweryExists_ShouldThrowInvalidOperationException()
-	{
-		var breweryName = $"Brewery{_testSuffix}";
-		var brewery = new Brewery { Name = breweryName, IsDeleted = false };
-
-		var breweryId = await _breweryDAO.CreateAsync(brewery);
-		_breweryIdsCreated.Add(breweryId);
-
-		var exception = Assert.ThrowsAsync<InvalidOperationException>(async () => await _breweryDAO.CreateAsync(brewery));
-		Assert.That(exception.Message, Does.Contain($"Brewery with name '{breweryName}' already exists."));
-	}
-	[Test]
-	public async Task DeleteBrewery_WhenBreweryExists_ShouldDeleteBrewery()
-	{
-		//Arrange
+    [Test]
+    public async Task CreateAsync_WhenBreweryDoesNotExist_ShouldCreateBrewery()
+    {
         var breweryName = $"Brewery{_testSuffix}";
-        var brewery = new Brewery { Name = breweryName, IsDeleted = false };
-		//Act
+        var brewery = new Brewery { Name = breweryName };
+
         var breweryId = await _breweryDAO.CreateAsync(brewery);
         _breweryIdsCreated.Add(breweryId);
-		var deleteResult = await _breweryDAO.DeleteAsync(breweryId);
+
+        Assert.That(breweryId, Is.GreaterThan(0));
+
+        var exception = Assert.ThrowsAsync<InvalidOperationException>(async () => await _breweryDAO.CreateAsync(brewery));
+        Assert.That(exception.Message, Is.EqualTo($"Brewery with name '{breweryName}' already exists."));
+    }
+
+    [Test]
+    public async Task CreateAsync_WhenBreweryExists_ShouldThrowInvalidOperationException()
+    {
+        var breweryName = $"Brewery{_testSuffix}";
+        var brewery = new Brewery { Name = breweryName };
+
+        var breweryId = await _breweryDAO.CreateAsync(brewery);
+        _breweryIdsCreated.Add(breweryId);
+
+        var exception = Assert.ThrowsAsync<InvalidOperationException>(async () => await _breweryDAO.CreateAsync(brewery));
+        Assert.That(exception.Message, Does.Contain($"Brewery with name '{breweryName}' already exists."));
+    }
+    [Test]
+    public async Task DeleteBrewery_WhenBreweryExists_ShouldDeleteBrewery()
+    {
+        //Arrange
+        var breweryName = $"Brewery{_testSuffix}";
+        var brewery = new Brewery { Name = breweryName };
+        //Act
+        var breweryId = await _breweryDAO.CreateAsync(brewery);
+        _breweryIdsCreated.Add(breweryId);
+        var deleteResult = await _breweryDAO.DeleteAsync(breweryId);
         //Assert
         Assert.IsTrue(deleteResult, "DeleteAsync should return true when a product is successfully deleted.");
         var deletedBrewery = await _breweryDAO.GetByIdAsync(breweryId);
         Assert.IsNull(deletedBrewery, "GetByIdAsync should return null for a deleted product.");
     }
-	[Test]
-	public async Task GetByIdAsync_WhenBreweryExists_ShouldReturnBrewery()
-	{
+    [Test]
+    public async Task GetByIdAsync_WhenBreweryExists_ShouldReturnBrewery()
+    {
         //Arrange
         var breweryName = $"Brewery{_testSuffix}";
-        var brewery = new Brewery { Name = breweryName, IsDeleted = false };
-		//Act
-		var breweryId = await _breweryDAO.CreateAsync(brewery);
-        _breweryIdsCreated.Add(breweryId);
-        var breweryReturned = await _breweryDAO.GetByIdAsync(breweryId);
-		//Assert
-		Assert.That(breweryReturned, Is.Not.Null);
-		Assert.That(brewery.Name, Is.EqualTo(breweryName));
-    }
-	[Test]
-	public async Task GetBreweryIdByName_WhenBreweryExists_ShouldReturnNameFromId()
-	{
-        //Arrange
-        var breweryName = $"Brewery{_testSuffix}";
-        var brewery = new Brewery { Name = breweryName, IsDeleted = false };
+        var brewery = new Brewery { Name = breweryName };
         //Act
         var breweryId = await _breweryDAO.CreateAsync(brewery);
         _breweryIdsCreated.Add(breweryId);
-		var breweryIdFromName = await _breweryDAO.GetBreweryIdByName(breweryName);
-		//Assert
-		Assert.That(breweryIdFromName, Is.EqualTo(breweryId));
+        var breweryReturned = await _breweryDAO.GetByIdAsync(breweryId);
+        //Assert
+        Assert.That(breweryReturned, Is.Not.Null);
+        Assert.That(brewery.Name, Is.EqualTo(breweryName));
+    }
+    [Test]
+    public async Task GetBreweryIdByName_WhenBreweryExists_ShouldReturnNameFromId()
+    {
+        //Arrange
+        var breweryName = $"Brewery{_testSuffix}";
+        var brewery = new Brewery { Name = breweryName };
+        //Act
+        var breweryId = await _breweryDAO.CreateAsync(brewery);
+        _breweryIdsCreated.Add(breweryId);
+        var breweryIdFromName = await _breweryDAO.GetBreweryIdByName(breweryName);
+        //Assert
+        Assert.That(breweryIdFromName, Is.EqualTo(breweryId));
 
     }
 }
--- BeerWebshop.Test/DALTests/ProductDaoTests.cs
+++ BeerWebshop.Test/DALTests/ProductDaoTests.cs
@@ -7,78 +7,78 @@ namespace BeerWebshop.Test.DALTests;
 [TestFixture]
 public class ProductDaoTests
 {
-	private ProductDAO _productDao;
-	private CategoryDAO _categoryDao;
-	private BreweryDAO _breweryDao;
+    private ProductDAO _productDao;
+    private CategoryDAO _categoryDao;
+    private BreweryDAO _breweryDao;
 
-	private readonly string _testSuffix = "_Test";
+    private readonly string _testSuffix = "_Test";
 
-	private int _createdCategoryId;
-	private int _createdBreweryId;
+    private int _createdCategoryId;
+    private int _createdBreweryId;
 
-	private List<int> _productIdsCreated = new();
-	private List<int> _breweryIdsCreated = new();
-	private List<int> _categoryIdsCreated = new();
+    private List<int> _productIdsCreated = new();
+    private List<int> _breweryIdsCreated = new();
+    private List<int> _categoryIdsCreated = new();
 
-	[SetUp]
-	public async Task SetUpAsync()
-	{
-		try
-		{
-			_productDao = new ProductDAO(DBConnection.ConnectionString());
-			_categoryDao = new CategoryDAO(DBConnection.ConnectionString());
-			_breweryDao = new BreweryDAO(DBConnection.ConnectionString());
+    [SetUp]
+    public async Task SetUpAsync()
+    {
+        try
+        {
+            _productDao = new ProductDAO(DBConnection.ConnectionString());
+            _categoryDao = new CategoryDAO(DBConnection.ConnectionString());
+            _breweryDao = new BreweryDAO(DBConnection.ConnectionString());
 
-			var category = new Category { Name = $"Category{_testSuffix}", IsDeleted = false };
-			_createdCategoryId = await _categoryDao.CreateAsync(category);
+            var category = new Category { Name = $"Category{_testSuffix}" };
+            _createdCategoryId = await _categoryDao.CreateAsync(category);
 
-			var brewery = new Brewery { Name = $"Brewery{_testSuffix}", IsDeleted = false };
-			_createdBreweryId = await _breweryDao.CreateAsync(brewery);
+            var brewery = new Brewery { Name = $"Brewery{_testSuffix}" };
+            _createdBreweryId = await _breweryDao.CreateAsync(brewery);
 
-		}
-		catch (Exception ex)
-		{
-			throw;
-		}
-
-
-	}
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
 
 
-	[TearDown]
-	public async Task TearDownAsync()
-	{
-		try
-		{
-		await DeleteAllProductsCreated();
-		await _categoryDao.DeleteAsync(_createdCategoryId);
-		await _breweryDao.DeleteAsync(_createdBreweryId);
-		_productIdsCreated.Clear();
-		}
-		catch (Exception ex)
-		{
-			throw;
-		}
+    }
 
 
-		//await DeleteAllCategoriesCreated();
-		//await DeleteAllBreweriesCreated();
-	}
+    [TearDown]
+    public async Task TearDownAsync()
+    {
+        try
+        {
+            await DeleteAllProductsCreated();
+            await _categoryDao.DeleteAsync(_createdCategoryId);
+            await _breweryDao.DeleteAsync(_createdBreweryId);
+            _productIdsCreated.Clear();
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+
+
+        //await DeleteAllCategoriesCreated();
+        //await DeleteAllBreweriesCreated();
+    }
 
     private async Task DeleteAllBreweriesCreated()
     {
-        foreach(var id in _breweryIdsCreated)
-		{
-			await _breweryDao.DeleteAsync(id);
-		}
+        foreach (var id in _breweryIdsCreated)
+        {
+            await _breweryDao.DeleteAsync(id);
+        }
     }
 
     private async Task DeleteAllCategoriesCreated()
     {
-        foreach(var id in _categoryIdsCreated)
-		{
-			await _categoryDao.DeleteAsync(id);
-		}
+        foreach (var id in _categoryIdsCreated)
+        {
+            await _categoryDao.DeleteAsync(id);
+        }
     }
 
     private async Task DeleteAllProductsCreated()
@@ -90,204 +90,199 @@ public class ProductDaoTests
     }
 
     [Test]
-	public async Task GetByIdAsync_WhenProductExists_ShouldReturnProduct()
-	{
-		var productId = await _productDao.CreateAsync(new Product
-		{
-			Name = $"Product{_testSuffix}",
-			Category = new Category { Id = _createdCategoryId },
-			Brewery = new Brewery { Id = _createdBreweryId },
-			Price = 80f,
-			Description = "Citrusy flavor.",
-			Stock = 5,
-			Abv = 6.5f,
-			ImageUrl = "http://example.com/image.jpg",
-			IsDeleted = false
-		});
+    public async Task GetByIdAsync_WhenProductExists_ShouldReturnProduct()
+    {
+        var productId = await _productDao.CreateAsync(new Product
+        {
+            Name = $"Product{_testSuffix}",
+            Category = new Category { Id = _createdCategoryId },
+            Brewery = new Brewery { Id = _createdBreweryId },
+            Price = 80f,
+            Description = "Citrusy flavor.",
+            Stock = 5,
+            Abv = 6.5f,
+            ImageUrl = "http://example.com/image.jpg"
+        });
 
-		_productIdsCreated.Add(productId);
-		
-		
+        _productIdsCreated.Add(productId);
 
-		var product = await _productDao.GetByIdAsync(productId);
 
-		Assert.IsNotNull(product);
-		Assert.That(product.Id, Is.EqualTo(productId));
-		Assert.That(product.Name, Is.EqualTo($"Product{_testSuffix}"));
-	}
 
-	[Test]
-	public async Task CreateAsync_WhenCalled_ShouldCreateProduct()
-	{
-		var product = new Product
-		{
-			Name = $"Product{_testSuffix}",
-			Category = new Category { Id = _createdCategoryId },
-			Brewery = new Brewery { Id = _createdBreweryId },
-			Price = 80f,
-			Description = "Citrusy flavor.",
-			Stock = 5,
-			Abv = 6.5f,
-			ImageUrl = "http://example.com/image.jpg",
-			IsDeleted = false
-		};
+        var product = await _productDao.GetByIdAsync(productId);
 
-		var productId = await _productDao.CreateAsync(product);
-		_productIdsCreated.Add(productId);
-        
+        Assert.IsNotNull(product);
+        Assert.That(product.Id, Is.EqualTo(productId));
+        Assert.That(product.Name, Is.EqualTo($"Product{_testSuffix}"));
+    }
+
+    [Test]
+    public async Task CreateAsync_WhenCalled_ShouldCreateProduct()
+    {
+        var product = new Product
+        {
+            Name = $"Product{_testSuffix}",
+            Category = new Category { Id = _createdCategoryId },
+            Brewery = new Brewery { Id = _createdBreweryId },
+            Price = 80f,
+            Description = "Citrusy flavor.",
+            Stock = 5,
+            Abv = 6.5f,
+            ImageUrl = "http://example.com/image.jpg",
+        };
+
+        var productId = await _productDao.CreateAsync(product);
+        _productIdsCreated.Add(productId);
+
 
         var createdProduct = await _productDao.GetByIdAsync(productId);
 
-		Assert.IsNotNull(createdProduct);
-		Assert.That(createdProduct.Name, Is.EqualTo(product.Name));
-	}
+        Assert.IsNotNull(createdProduct);
+        Assert.That(createdProduct.Name, Is.EqualTo(product.Name));
+    }
 
-	[Test]
-	public async Task UpdateAsync_WhenProductUpdated_ShouldReflectChanges()
-	{
-		var productId = await _productDao.CreateAsync(new Product
-		{
-			Name = $"OriginalProduct{_testSuffix}",
-			Category = new Category { Id = _createdCategoryId },
-			Brewery = new Brewery { Id = _createdBreweryId },
-			Price = 50f,
-			Description = "Original description.",
-			Stock = 10,
-			Abv = 5.0f,
-			ImageUrl = "http://example.com/original.jpg",
-			IsDeleted = false
-		});
+    [Test]
+    public async Task UpdateAsync_WhenProductUpdated_ShouldReflectChanges()
+    {
+        var productId = await _productDao.CreateAsync(new Product
+        {
+            Name = $"OriginalProduct{_testSuffix}",
+            Category = new Category { Id = _createdCategoryId },
+            Brewery = new Brewery { Id = _createdBreweryId },
+            Price = 50f,
+            Description = "Original description.",
+            Stock = 10,
+            Abv = 5.0f,
+            ImageUrl = "http://example.com/original.jpg",
+        });
 
-		_productIdsCreated.Add(productId);
-       
+        _productIdsCreated.Add(productId);
 
-        var product = await _productDao.GetByIdAsync(productId);
-		product.Name = $"UpdatedProduct{_testSuffix}";
-		product.Price = 60f;
-
-		var updateResult = await _productDao.UpdateAsync(product);
-
-		Assert.IsTrue(updateResult);
-
-		var updatedProduct = await _productDao.GetByIdAsync(productId);
-
-		Assert.That(updatedProduct.Name, Is.EqualTo(product.Name));
-		Assert.That(updatedProduct.Price, Is.EqualTo(product.Price));
-	}
-
-	[Test]
-	public async Task UpdateAsync_WhenRowVersionModified_ShouldThrowConcurrencyException()
-	{
-		var productId = await _productDao.CreateAsync(new Product
-		{
-			Name = $"OriginalProduct{_testSuffix}",
-			Category = new Category { Id = _createdCategoryId },
-			Brewery = new Brewery { Id = _createdBreweryId },
-			Price = 50f,
-			Description = "Original description.",
-			Stock = 10,
-			Abv = 5.0f,
-			ImageUrl = "http://example.com/original.jpg",
-			IsDeleted = false
-		});
-
-		_productIdsCreated.Add(productId);
-        
 
         var product = await _productDao.GetByIdAsync(productId);
-		var originalRowVersion = product.RowVersion;
+        product.Name = $"UpdatedProduct{_testSuffix}";
+        product.Price = 60f;
 
-		product.Name = $"UpdatedProduct{_testSuffix}";
-		await _productDao.UpdateAsync(product);
+        var updateResult = await _productDao.UpdateAsync(product);
 
-		product.RowVersion = originalRowVersion;
+        Assert.IsTrue(updateResult);
 
-		var ex = Assert.ThrowsAsync<Exception>(async () => await _productDao.UpdateAsync(product));
-		Assert.That(ex.Message, Is.EqualTo("Error updating product: Concurrency conflict detected."));
-	}
+        var updatedProduct = await _productDao.GetByIdAsync(productId);
 
-	[Test]
-	public async Task GetProductsAsync_WithAscendingNameOrder()
-	{
-		var queryParameters = new ProductQueryParameters { SortBy = "nameAsc" };
-		var products = await _productDao.GetProducts(queryParameters);
+        Assert.That(updatedProduct.Name, Is.EqualTo(product.Name));
+        Assert.That(updatedProduct.Price, Is.EqualTo(product.Price));
+    }
 
-		Assert.That(products, Is.Not.Null);
-		Assert.That(products.Count(), Is.LessThanOrEqualTo(21));
+    [Test]
+    public async Task UpdateAsync_WhenRowVersionModified_ShouldThrowConcurrencyException()
+    {
+        var productId = await _productDao.CreateAsync(new Product
+        {
+            Name = $"OriginalProduct{_testSuffix}",
+            Category = new Category { Id = _createdCategoryId },
+            Brewery = new Brewery { Id = _createdBreweryId },
+            Price = 50f,
+            Description = "Original description.",
+            Stock = 10,
+            Abv = 5.0f,
+            ImageUrl = "http://example.com/original.jpg",
+        });
 
-		var sortedProducts = products.OrderBy(p => p.Name).ToList();
-		Assert.That(products.SequenceEqual(sortedProducts));
-	}
+        _productIdsCreated.Add(productId);
 
-	[Test]
-	public async Task GetProductsAsync_WithDescendingNameOrder()
-	{
-		var queryParameters = new ProductQueryParameters { SortBy = "nameDesc" };
-		var products = await _productDao.GetProducts(queryParameters);
 
-		Assert.That(products, Is.Not.Null);
-		Assert.That(products.Count(), Is.LessThanOrEqualTo(21));
+        var product = await _productDao.GetByIdAsync(productId);
+        var originalRowVersion = product.RowVersion;
 
-		var sortedProducts = products.OrderByDescending(p => p.Name).ToList();
-		Assert.That(products.SequenceEqual(sortedProducts));
-	}
+        product.Name = $"UpdatedProduct{_testSuffix}";
+        await _productDao.UpdateAsync(product);
 
-	[Test]
-	public async Task GetProductsAsync_WithAscendingPriceOrder()
-	{
-		var queryParameters = new ProductQueryParameters { SortBy = "priceAsc" };
-		var products = await _productDao.GetProducts(queryParameters);
+        product.RowVersion = originalRowVersion;
 
-		Assert.That(products, Is.Not.Null);
-		Assert.That(products.Count(), Is.LessThanOrEqualTo(21));
+        var ex = Assert.ThrowsAsync<Exception>(async () => await _productDao.UpdateAsync(product));
+        Assert.That(ex.Message, Is.EqualTo("Error updating product: Concurrency conflict detected."));
+    }
 
-		var sortedProducts = products.OrderBy(p => p.Price).ToList();
-		Assert.That(products.SequenceEqual(sortedProducts));
-	}
+    [Test]
+    public async Task GetProductsAsync_WithAscendingNameOrder()
+    {
+        var queryParameters = new ProductQueryParameters { SortBy = "nameAsc" };
+        var products = await _productDao.GetProducts(queryParameters);
 
-	[Test]
-	public async Task GetProductsAsync_WithDescendingPriceOrder()
-	{
-		var queryParameters = new ProductQueryParameters { SortBy = "priceDesc" };
-		var products = await _productDao.GetProducts(queryParameters);
+        Assert.That(products, Is.Not.Null);
+        Assert.That(products.Count(), Is.LessThanOrEqualTo(21));
 
-		Assert.That(products, Is.Not.Null);
-		Assert.That(products.Count(), Is.LessThanOrEqualTo(21));
+        var sortedProducts = products.OrderBy(p => p.Name).ToList();
+        Assert.That(products.SequenceEqual(sortedProducts));
+    }
 
-		var sortedProducts = products.OrderByDescending(p => p.Price).ToList();
-		Assert.That(products.SequenceEqual(sortedProducts));
-	}
+    [Test]
+    public async Task GetProductsAsync_WithDescendingNameOrder()
+    {
+        var queryParameters = new ProductQueryParameters { SortBy = "nameDesc" };
+        var products = await _productDao.GetProducts(queryParameters);
 
-	[Test]
-	public async Task CreateAsync_WhenDuplicateName_ShouldThrowException()
-	{
-		var productName = $"DuplicateProduct{_testSuffix}";
+        Assert.That(products, Is.Not.Null);
+        Assert.That(products.Count(), Is.LessThanOrEqualTo(21));
 
-		var product = new Product
-		{
-			Name = productName,
-			Category = new Category { Id = _createdCategoryId },
-			Brewery = new Brewery { Id = _createdBreweryId },
-			Price = 80f,
-			Description = "Citrusy flavor.",
-			Stock = 5,
-			Abv = 6.5f,
-			ImageUrl = "http://example.com/image.jpg",
-			IsDeleted = false
-		};
+        var sortedProducts = products.OrderByDescending(p => p.Name).ToList();
+        Assert.That(products.SequenceEqual(sortedProducts));
+    }
 
-		var productId = await _productDao.CreateAsync(product);
-		_productIdsCreated.Add(productId);
-        
+    [Test]
+    public async Task GetProductsAsync_WithAscendingPriceOrder()
+    {
+        var queryParameters = new ProductQueryParameters { SortBy = "priceAsc" };
+        var products = await _productDao.GetProducts(queryParameters);
+
+        Assert.That(products, Is.Not.Null);
+        Assert.That(products.Count(), Is.LessThanOrEqualTo(21));
+
+        var sortedProducts = products.OrderBy(p => p.Price).ToList();
+        Assert.That(products.SequenceEqual(sortedProducts));
+    }
+
+    [Test]
+    public async Task GetProductsAsync_WithDescendingPriceOrder()
+    {
+        var queryParameters = new ProductQueryParameters { SortBy = "priceDesc" };
+        var products = await _productDao.GetProducts(queryParameters);
+
+        Assert.That(products, Is.Not.Null);
+        Assert.That(products.Count(), Is.LessThanOrEqualTo(21));
+
+        var sortedProducts = products.OrderByDescending(p => p.Price).ToList();
+        Assert.That(products.SequenceEqual(sortedProducts));
+    }
+
+    [Test]
+    public async Task CreateAsync_WhenDuplicateName_ShouldThrowException()
+    {
+        var productName = $"DuplicateProduct{_testSuffix}";
+
+        var product = new Product
+        {
+            Name = productName,
+            Category = new Category { Id = _createdCategoryId },
+            Brewery = new Brewery { Id = _createdBreweryId },
+            Price = 80f,
+            Description = "Citrusy flavor.",
+            Stock = 5,
+            Abv = 6.5f,
+            ImageUrl = "http://example.com/image.jpg",
+        };
+
+        var productId = await _productDao.CreateAsync(product);
+        _productIdsCreated.Add(productId);
+
 
         var ex = Assert.ThrowsAsync<InvalidOperationException>(async () => await _productDao.CreateAsync(product));
 
-		Assert.That(ex.Message, Does.Contain($"A product with the name '{productName}' already exists."));
-	}
-	[Test]
-	public async Task DeleteAsync_WhenProductExists_ShouldRemoveProduct()
-	{
-		//Arrange
+        Assert.That(ex.Message, Does.Contain($"A product with the name '{productName}' already exists."));
+    }
+    [Test]
+    public async Task DeleteAsync_WhenProductExists_ShouldRemoveProduct()
+    {
+        //Arrange
         var productName = $"DuplicateProduct{_testSuffix}";
         var product = new Product
         {
@@ -299,13 +294,12 @@ public class ProductDaoTests
             Stock = 5,
             Abv = 6.5f,
             ImageUrl = "http://example.com/image.jpg",
-            IsDeleted = false
         };
-		//Act
+        //Act
         var productId = await _productDao.CreateAsync(product);
         _productIdsCreated.Add(productId);
         var deleteResult = await _productDao.DeleteAsync(productId);
-       
+
         //Assert
         Assert.IsTrue(deleteResult, "DeleteAsync should return true when a product is successfully deleted.");
         var deletedProduct = await _productDao.GetByIdAsync(productId);
